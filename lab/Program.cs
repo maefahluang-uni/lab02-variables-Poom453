6a888012febe7827c80f1257213a211d4974c565
Poom453 <164710138+Poom453@users.noreply.github.com>
2025-08-01 04:28:30 +0000

    test2

--- lab/Program.cs
+++ lab/Program.cs
@@ -5,7 +5,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        // write your code that converts 350 degree of fahrentheit to celsuis degree.
+
         string? input = Console.ReadLine();
         int fahrentheit = Convert.ToInt32(input);
         double celsuis = (fahrentheit - 32) * 5/9;
